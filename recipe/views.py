eb2d6f619f76b64ed899d3c9e40abfce48e317d1
phpusr <phpusr@gmail.com>
2020-01-16 19:18:57 +0500

    add: List ingredients feature

--- recipe/views.py
+++ recipe/views.py
@@ -21,3 +21,15 @@ class TagViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         """Create a new tag"""
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage ingredients in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = models.Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Returns objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
