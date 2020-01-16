cd05ef90f373cd116cd32e28fbc3b9a8f0e491fe
phpusr <phpusr@gmail.com>
2020-01-16 18:08:55 +0500

    change: Disable stdout in test_commands.py

--- core/tests/test_commands.py
+++ core/tests/test_commands.py
@@ -1,11 +1,16 @@
+import os
 from unittest.mock import patch
 
+import sys
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import TestCase
 
 
 class CommandTests(TestCase):
+    def setUp(self):
+        sys.stdout = open(os.devnull, 'w')
+
     def test_wait_for_db_ready(self):
         """Test waiting for db when DB is available"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
